8d9a6317d3a4de03fe6ae1497ea5a30fdaa6b1fd
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-05-28 16:47:13 -0600

    Add basic init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import format from 'date-fns/format';
+import Project from './components/project';
+import ProjectManager from './components/projectManager';
+import Todo from './components/todo';
+import {TodoUi, ProjectUi, WindowUi} from './components/ui';
+
+window.onload(WindowUi.initLoad);
+
