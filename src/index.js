e1b9e7b3792d297d3af1872ce6ee2544e3441997
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-05-30 15:06:52 -0600

    Add localStorage import statement

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Project from './components/project';
 import ProjectManager from './components/projectManager';
 import Todo from './components/todo';
 import {TodoUi, ProjectUi, WindowUi} from './components/ui';
+import LocalStorage from './components/localStorage';
 
 WindowUi.initLoad();
 
